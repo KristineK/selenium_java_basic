d0904436b94b07ac609772ae9b660ab60eaea686
Kristine <kristine.kronberga@accenture.com>
2017-08-02 17:19:49 +0300

    function

--- src/selenium/sample3/Sample3.java
+++ src/selenium/sample3/Sample3.java
@@ -2,12 +2,14 @@ package selenium.sample3;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.ComparisonFailure;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class Sample3 {
     WebDriver driver;
@@ -52,12 +54,63 @@ public class Sample3 {
         assertEquals(expectedNumberOfElements, actualNumberOfElements);
     }
 
-    
+    @Test
+    public void assertTrueExamples() throws Exception {
+        String elementTextOnPage = driver.findElement(By.cssSelector(".unbelievable")).getText();
+        assertTrue(elementTextOnPage.equals("unbelievable sample text"));
+        assertTrue(elementTextOnPage.contains("ample"));
+        assertTrue(elementTextOnPage.equalsIgnoreCase("unBELIEVable sAMPle Text"));
+        // pass:
+        assertTrue(true);
+        // fail:
+//        assertTrue(false);
+    }
 
-//            assertTrueExample (assertTrue with “String.equals()”, “String.contains()”, “String. equalsIgnoreCase()”)
-//    assertFalseExample (assertFalse with “!String.equals()”, “String.equals(“not”)”, “String. contains(“not”)”)
-//    failExampleWithDefaultError (see how fail() works)
-//    failExampleWithCustomError (see how fail(“w/m”) works)
-//    assertEqualsExampleWithDefaultError (try { assertEquals(exp, act) } catch (ComparisonFailure e) { System.err.println(); e.printStackTrace(); } )
-//    assertEqualsExampleWithCustomError (try { assertEquals(“custom message”, ext, act) } catch (ComparisonFailure e) { System.err.println(); e.printStackTrace(); }) Push all your changes to git
+    @Test
+    public void assertFalseExample() throws Exception {
+        String elementTextOnPage = driver.findElement(By.cssSelector(".unbelievable")).getText();
+        assertFalse(!elementTextOnPage.equals("unbelievable sample text"));
+        assertFalse(elementTextOnPage.equals("wrong text"));
+        assertFalse(elementTextOnPage.contains("wrong text"));
+        // fail:
+//        assertFalse(true);
+        // pass:
+        assertFalse(false);
+    }
+
+    @Test
+    public void failExampleWithDefaultError() throws Exception {
+        fail();
+        // will throw an java.lang.AssertionError
+    }
+
+    @Test
+    public void failExampleWithCustomError() throws Exception {
+        fail("I want this test to fail, so will!");
+        // will throw an java.lang.AssertionError: I want this test to fail, so will!
+    }
+
+    @Test
+    public void assertEqualsExampleWithDefaultError() throws Exception {
+        try {
+            assertEquals(3, 4);
+        } catch (AssertionError e) {
+            System.err.println("We failed ");
+            e.printStackTrace();
+//            We failed
+//            java.lang.AssertionError: expected:<3> but was:<4>
+        }
+    }
+
+    @Test
+    public void assertEqualsExampleWithCustomError() throws Exception {
+        try {
+            assertEquals("custom message", 3, 4);
+        } catch (AssertionError e) {
+            System.err.println("We failed with custom message”");
+            e.printStackTrace();
+        }
+//        We failed with custom message”
+//        java.lang.AssertionError: custom message expected:<3> but was:<4>
+    }
 }
