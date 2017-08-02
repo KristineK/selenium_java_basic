9306d04d7b399ceed9739e361ea3462aa89a894f
Student <konstantin.pletenev@accenture.com>
2017-08-02 11:18:15 +0300

    new filel

---
+++ src/Smaple2.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by Student on 8/2/2017.
+ */
+public class Smaple2 {
+    public void findElementById(){
+        String libWithDriversLocation =  System.getProperty("user.dir") + "\\lib\\";
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://kristinek.github.io/test-sample/examples/loc");
+
+    }
+    public void findElementByName(){
+
+    }
+    public void findElementByTagName(){
+
+    }
+    public void findElementByClassFirst(){
+
+    }
+}
