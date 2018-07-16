51fcfbd63b142d3ad61545aa15c0dac3599b368c
Kristine <kristine.kronberga@accenture.com>
2018-07-16 16:10:55 +0300

    sample 1 task todo

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -10,14 +10,11 @@ public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
-        //define driver
-
-        // go to https://kristinek.github.io/test-sample/index2.html
-
-        // get title of page
-
-        // get URL of current page
-
-        //Close browser
+//         TODO:
+//         define driver
+//         go to https://kristinek.github.io/test-sample/index2.html
+//         get title of page
+//         get URL of current page
+//         close browser
     }
 }
