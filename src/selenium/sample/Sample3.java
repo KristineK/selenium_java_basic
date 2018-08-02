74c3e047cf22ecb13fa01430b41987d8a749d6a5
kristine <kristinek@github.com>
2018-08-02 14:01:17 +0300

    adding sample 3

--- src/selenium/sample/Sample3.java
+++ src/selenium/sample/Sample3.java
@@ -63,7 +63,7 @@ public class Sample3 {
     public void assertTrueExamples() throws Exception {
         String elementTextOnPage = driver.findElement(By.cssSelector(".unbelievable")).getText();
         assertTrue(elementTextOnPage.equals("unbelievable sample text"));
-        assertTrue(elementTextOnPage.contains("ample"));
+        assertTrue("Expecting the element with class 'unbelievable' to have text 'ajshdka'", elementTextOnPage.contains("ajshdka"));
         assertTrue(elementTextOnPage.equalsIgnoreCase("unBELIEVable sAMPle Text"));
         // pass:
         assertTrue(true);
