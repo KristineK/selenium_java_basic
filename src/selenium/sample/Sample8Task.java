d6280715f1e08052c18e655b18a121e04728ee69
kristine <kristinek@github.com>
2018-08-03 15:32:13 +0300

    sample 8 task 1

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -34,6 +34,9 @@ public class Sample8Task {
 
     @Test
     public void styleChecks() throws Exception {
+        assertEquals("rgba(255, 221, 221, 1)",
+                driver.findElement(By.xpath("//*[@class='w3-container w3-pale-red']"))
+                        .getCssValue("background-color"));
 //         TODO:
 //        check the background of top 2 sections
 //        check h1 element font-size 64px
