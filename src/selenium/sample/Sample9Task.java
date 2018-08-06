1ac37558d0e9134b81c185f6d783f7340d60a569
kristine <kristinek@github.com>
2018-08-06 11:13:25 +0300

    renaming sample9 task, part 2

--- src/selenium/sample/Sample9Task.java
+++ src/selenium/sample/Sample9Task.java
@@ -5,24 +5,28 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class Sample9Task {
     WebDriver driver;
 
     @Before
-    public void openPage(){
-        String libWithDriversLocation =  System.getProperty("user.dir") + "\\lib\\";
+    public void openPage() {
+        String libWithDriversLocation = System.getProperty("user.dir") + "\\lib\\";
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://kristinek.github.io/test-sample/tasks/task2");
     }
 
     @After
-    public void closeBrowser(){
+    public void closeBrowser() {
         driver.close();
     }
 
@@ -48,6 +52,16 @@ public class Sample9Task {
 
     @Test
     public void loadGreenImplicit() throws Exception {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("start_green")).click();
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertEquals(driver.findElement(By.id("loading_green")).getText(), "Loading green...");
+
+        WebElement myGreen = driver.findElement(By.id("finish_green"));
+        assertTrue(myGreen.isDisplayed());
+        assertFalse(driver.findElement(By.id("start_green")).isDisplayed());
+        assertFalse(driver.findElement(By.id("loading_green")).isDisplayed());
+        assertEquals(myGreen.getText(), "Green Loaded");
         /* TODO:
          * 1) click on start loading green button
          * 2) check that button does not appear,
@@ -71,16 +85,16 @@ public class Sample9Task {
     }
 
     @Test
-    public void loadGreenAndBlueBonus(){
-		/* TODO:
-		 * 0) wait until button to load green and blue appears
-		 * 1) click on start loading green and blue button
-		 * 2) check that button does not appear, but loading text is seen instead for green
-		 * 3) check that button does not appear, but loading text is seen instead for green and blue
-		 * 4) check that button and loading green does not appear,
-		 * 		but loading text is seen instead for blue and success for green is seen
-		 * 5) check that both button and loading text is not seen, success is seen instead
-		 */
+    public void loadGreenAndBlueBonus() {
+        /* TODO:
+         * 0) wait until button to load green and blue appears
+         * 1) click on start loading green and blue button
+         * 2) check that button does not appear, but loading text is seen instead for green
+         * 3) check that button does not appear, but loading text is seen instead for green and blue
+         * 4) check that button and loading green does not appear,
+         * 		but loading text is seen instead for blue and success for green is seen
+         * 5) check that both button and loading text is not seen, success is seen instead
+         */
     }
 
 }
