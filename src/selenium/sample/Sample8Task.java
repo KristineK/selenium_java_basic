d562f5e7d08f1d32eb955e7d7fb42a8a66d04588
kristine <kristinek@github.com>
2018-08-06 09:03:15 +0300

    sample 8 task 3

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -34,14 +34,14 @@ public class Sample8Task {
 
     @Test
     public void styleChecks() throws Exception {
+//        check the background of top 2 sections
         assertEquals("rgba(255, 221, 221, 1)",
                 driver.findElement(By.xpath("//*[@class='w3-container w3-pale-red']"))
                         .getCssValue("background-color"));
         assertEquals("rgba(255, 255, 204, 1)",
                 driver.findElement(By.xpath("//*[@class='w3-container w3-pale-yellow']"))
                         .getCssValue("background-color"));
-//         TODO:
-//        check the background of top 2 sections
 //        check h1 element font-size 64px
+        assertEquals("64px", driver.findElement(By.xpath("//h1")).getCssValue("font-size"));
     }
 }
