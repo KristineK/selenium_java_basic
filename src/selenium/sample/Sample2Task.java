97448117ff29ae13a27f92ebff67a902eda57058
kristine <kristinek@github.com>
2018-11-26 11:22:46 +0200

    task 2

--- src/selenium/sample/Sample2Task.java
+++ src/selenium/sample/Sample2Task.java
@@ -3,8 +3,12 @@ package selenium.sample;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class Sample2Task {
     WebDriver driver;
@@ -33,6 +37,7 @@ public class Sample2Task {
 //         TODO:
 //         get text "Heading 2 text" using id
 //        <h2 id="heading_2">Heading 2 text</h2>
+        System.out.println(driver.findElement(By.id("heading_2")).getText());
     }
 
     @Test
@@ -40,16 +45,30 @@ public class Sample2Task {
 //         TODO:
 //         get attribute "id" and "value" of button "This is also a button" using name
 //        <input type="button" id="buttonId" value="This is also a button" name="randomButton2">
+        System.out.println(driver.findElement(By.name("randomButton2")).getAttribute("id") + " -- " +  driver.findElement(By.name("randomButton2")).getAttribute("value"));
     }
 
     @Test
     public void findElementByClassFirst() throws Exception {
 //         TODO:
 //         get first text of class "test" (should be "Test Text 1")
+        System.out.println(driver.findElement(By.className("test")).getText());
     }
 
     @Test
     public void findElementByClassAll() throws Exception {
+        List<WebElement> allElementWithClass = driver.findElements(By.className("test"));
+        System.out.println(allElementWithClass.size());
+//        same as line above:
+//      System.out.println(driver.findElements(By.className("test")).size());
+//        for (int i = 0; i < allElementWithClass.size(); i++) {
+//             allElementWithClass.get(i).getText();
+// }
+        for (WebElement elem : allElementWithClass) {
+            System.out.println(elem.getText());
+        }
+        System.out.println(allElementWithClass.get(2).getText());
+
 //         TODO:
 //         get size text of class "test" (should be 5)
 //         get text of class "test"
