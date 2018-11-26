7bf905109f451cdba75f1cc1ad6eb6a9ce519f1a
kristine <kristinek@github.com>
2018-11-26 10:55:59 +0200

    task 1

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -10,18 +10,17 @@ public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
+//         define driver
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
         WebDriver d = new ChromeDriver();
+//         go to https://kristinek.github.io/site/index2.html
         d.get("https://kristinek.github.io/site/index2.html");
+//         get title of page
         System.out.println(d.getTitle());
+//         get URL of current page
         System.out.println(d.getCurrentUrl());
 //        sout
-        d.close();
-//         TODO:
-//         define driver
-//         go to https://kristinek.github.io/site/index2.html
-//         get title of page
-//         get URL of current page
 //         close browser
+        d.close();
     }
 }
