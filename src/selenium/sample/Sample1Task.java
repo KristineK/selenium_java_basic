30f509331c2596974a3d3858b066ee04cffcac0e
garrilase@inbox.lv <Yamakasi_123>
2020-10-26 11:39:06 +0200

    Completed Sample1Task

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -2,6 +2,7 @@ package selenium.sample;
 
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,11 +11,27 @@ public class Sample1Task {
 
     @Test
     public void goToHomepage() throws Exception {
-//        TODO:
-//         define driver
-//         go to https://kristinek.github.io/site/index2.html
-//         get title of page
-//         get URL of current page
-//         close browser
+
+        /*
+        TODO:
+        define driver
+        go to https://kristinek.github.io/site/index2.html
+        get title of page
+        get URL of current page
+        close browser
+        */
+
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://kristinek.github.io/site/index2.html");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        Thread.sleep(10000);
+
+        driver.quit();
+
     }
 }
